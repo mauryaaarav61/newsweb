0e924601a39a18228fb75f8b8f947610b2b4dec7
Rakesh kumar <mauryaaarav61@gmail.com>
2023-12-17 11:32:10 -0800

    updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 async function fetchNews(news) {
   const apiKey = "1a97b38b931648cf924bcdffcb983ea2";
   const apiUrl = `https://newsapi.org/v2/everything?q=${news}&apiKey=${apiKey}`;
@@ -6,11 +7,15 @@ async function fetchNews(news) {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    displayNews(data.articles);
+    if (data.articles && Array.isArray(data.articles)) {
+      
+      displayNews(data.articles);
 
-    //  search par upadate  //
-
-    updateHeader(news);
+     
+      updateHeader(news);
+    } else {
+      console.error("Invalid data structure:", data);
+    }
 
   } catch (error) {
     console.error("Error fetching news:", error);
@@ -52,46 +57,6 @@ function displayNews(articles) {
   });
 }
 
-function displayNews(articles) {
-  const newsContainer = document.getElementById("news_paragraph");
-  newsContainer.innerHTML = "";
-
-  const newsCards = articles.map((article) => {
-    const newsCard = document.createElement("div");
-    newsCard.classList.add("news-card");
-
-    const publishedDate = new Date(article.publishedAt);
-    const customDate = `${publishedDate.getFullYear()}-${
-      publishedDate.getMonth() + 1
-    }-${publishedDate.getDate()}`;
-
-    newsCard.innerHTML = `
-      <h2>${article.title}</h2>
-      ${
-        article.urlToImage
-          ? `<img src="${article.urlToImage}" alt="News Image">`
-          : ""
-      }
-      <p>${article.description}</p>
-      <h3>${article.author}</h3>
-      <h3>${article.source.name}</h3>
-      <div class="published">
-        <p>Published on :</p>
-        <h4> ${customDate}</h4>
-      </div>
-      <pre onclick="window.open('${article.url}', '_blank')">Read More -->></pre>
-    `;
-
-    return newsCard;
-  });
-
-  newsCards.forEach((newsCard) => {
-    newsContainer.appendChild(newsCard);
-  });
-}
-
-
-
 function updateHeader(news) {
   const header = document.getElementById("subject");
   header.textContent = ` ${news}`;
@@ -107,7 +72,8 @@ async function searchNews() {
   if (news.trim() !== "") {
     await fetchNews(news);
 
-    searchInput.value = "";
+   
+    document.getElementById("searchInput").value = "";
   }
 }
 
@@ -115,10 +81,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const currentYear = new Date().getFullYear();
   const copyrightSection = document.getElementById("copyright-section");
   if (copyrightSection) {
-    copyrightSection.innerHTML = `<p>&copy; ${currentYear} Universal News.All Rights Reserved.</p>`;
+    copyrightSection.innerHTML = `<p>&copy; ${currentYear} Universal News. All Rights Reserved.</p>`;
   }
 });
 
-// default load 
-
+// default load
 window.onload = fetchDefaultNews;
+
